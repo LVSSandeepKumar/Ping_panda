5353c17473982f8d79c957f9bb990cb1e890142d
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-11-13 14:18:24 +0530

    Create sidebar for dashboard page

--- src/components/dashboard-page.tsx
+++ src/components/dashboard-page.tsx
@@ -19,7 +19,8 @@ const DashboardPage = ({
   return (
     <section className="flex-1 w-full h-full flex flex-col">
       <div className="p-6 sm:p-8 flex justify-between border-b border-gray-200">
-        <div className="flex flex-col sm:flex-row sm:items-center gap-y-2 gap-x-8">
+        <div className="flex flex-col items-start sm:flex-row sm:items-center gap-6">
+          <div className="flex items-center gap-6">
           {hideBackButton ? null : (
             <Button className="w-fit bg-white" variant="outline">
               <ArrowLeft className="size-4" />
@@ -27,6 +28,7 @@ const DashboardPage = ({
           )}
 
           <Heading>{title}</Heading>
+          </div>
 
           {cta ? <div>{cta}</div> : null}
         </div>
