3e22ba3ce84749f6756cf19578b934166536d5a0
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-11-14 17:49:18 +0530

    Initiate API for tracking events

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const FREE_QUOTA = {
+    maxEventsPerMonth : 200,
+    maxEventCategories: 3
+} as const
+
+export const PRO_QUOTA = {
+    maxEventsPerMonth : 2000,
+    maxEventCategories: 10
+} as const
